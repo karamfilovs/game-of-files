5a6a7fb7abf589969fd0cf67b688e183f0d41e32
Alex Karamfilov <karamfilovs@gmail.com>
2023-05-24 19:48:59 +0300

    Added FTPService

--- src/test/java/FTPService.java
+++ src/test/java/FTPService.java
@@ -1,2 +1,59 @@
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+
 public class FTPService {
+
+    public FTPClient loginFtp(String host, int port, String username, String password) throws Exception {
+        FTPClient ftpClient = new FTPClient();
+        ftpClient.connect(host, port);
+        ftpClient.login(username, password);
+        return ftpClient;
+    }
+
+    public void createDirectory(String path, FTPClient ftpClient) throws Exception {
+        System.out.println();
+        System.out.printf("[createDirectory][%d] Is success to create directory : %s -> %b",
+                System.currentTimeMillis(), path, ftpClient.makeDirectory(path));
+        System.out.println();
+    }
+
+    public void uploadFile(String localPath, String remotePath, FTPClient ftpClient) throws Exception {
+        FileInputStream fileInputStream = new FileInputStream(localPath);
+        System.out.println();
+        System.out.printf("[uploadFile][%d] Is success to upload file : %s -> %b",
+                System.currentTimeMillis(), remotePath, ftpClient.storeFile(remotePath, fileInputStream));
+        System.out.println();
+    }
+
+    public void renameFile(String oldPath, String newPath, FTPClient ftpClient) throws Exception {
+        System.out.println();
+        System.out.printf("[renameFile][%d] Is success to rename file : from %s to %s -> %b",
+                System.currentTimeMillis(), oldPath, newPath, ftpClient.rename(oldPath, newPath));
+        System.out.println();
+    }
+
+    public byte[] downloadFile(String path, FTPClient ftpClient) throws Exception {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.out.println();
+        System.out.printf("[downloadFile][%d] Is success to download file : %s -> %b",
+                System.currentTimeMillis(), path, ftpClient.retrieveFile(path, byteArrayOutputStream));
+        System.out.println();
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public void deleteFile(String path, FTPClient ftpClient) throws Exception {
+        System.out.println();
+        System.out.printf("[deleteFile][%d] Is success to delete file : %s -> %b",
+                System.currentTimeMillis(), path, ftpClient.deleteFile(path));
+        System.out.println();
+    }
+
+    public void deleteDirectory(String path, FTPClient ftpClient) throws Exception {
+        System.out.println();
+        System.out.printf("[deleteDirectory][%d] Is success to delete directory : %s -> %b",
+                System.currentTimeMillis(), path, ftpClient.removeDirectory(path));
+        System.out.println();
+    }
 }
